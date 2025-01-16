ad9a599adde8a128fa357d90c507e5d9dc721ecc
brochjosias <brochjosias@gmail.com>
2025-01-16 20:31:30 -0300

    Funcoes CRUD
    -Criacao de tarefas
    -edição
    -salvamento
    -abrir a criacao e fechar
    -marcar como feito
    -bootstrap para estilização
    -e demais estilizações

--- ListaDeTarefas/src/app/components/tasks/tasks.component.ts
+++ ListaDeTarefas/src/app/components/tasks/tasks.component.ts
@@ -3,10 +3,11 @@ import { TaskService } from '../../services/task.service';
 import { Tarefa } from '../../../Tarefa';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -22,11 +23,22 @@ export class TasksComponent implements OnInit {
     });
   }
 
+  AddTask(tarefa: Tarefa) {
+    this.taskService.addTask(tarefa).subscribe((tarefa) => {
+      this.tarefas.push(tarefa);
+    });
+  }
+
   deleteTask(tarefa: Tarefa) {
     this.taskService
       .deleteTask(tarefa)
       .subscribe(
         () => (this.tarefas = this.tarefas.filter((t) => t.id !== tarefa.id))
       );
+  }
+
+  toggleConcluiido(tarefa: Tarefa) {
+    tarefa.concluido = !tarefa.concluido;
+    this.taskService.updateTask(tarefa).subscribe();
   }
 }
